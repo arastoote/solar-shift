524f43b2217abaf2d384bb47e1385b4eba0de996
nick-gorman <n.gorman305@gmail.com>
2025-03-06 16:16:10 +1100

    basic version

--- main.py
+++
@@ -1,6 +0,0 @@
-def main():
-    print("Hello from solar-shift!")
-
-
-if __name__ == "__main__":
-    main()
--- solar_shift.py
+++ solar_shift.py
@@ -1,13 +1,13 @@
 import streamlit as st
+from streamlit.components.v1 import html
 import pandas as pd
 import plotly.express as px
+import time
 
 st.set_page_config(
     page_title="Explore!",
     layout="wide"
 )
-
-left_col, middle_col, right_col = st.columns([1, 2, 1])
 
 @st.cache_data
 def get_data():
@@ -15,38 +15,132 @@ def get_data():
 
 data = get_data()
 
-with left_col:
-    st.subheader("Dive into")
-    hs = st.multiselect("Household size", data["household_size"].unique())
-    locs = st.multiselect("Location", data["location"].unique())
-    st.subheader("Compare across")
-    groups = []
-    if st.checkbox("Heater type", value=False):
-        groups.append("heater_type")
+groups = [
+    "location",
+    "household_size",
+    "heater_type",
+    "tariff_type",
+    "control_type",
+    "has_solar"
+]
 
-f_data = data.copy()
+home, explore, compare = st.tabs(["Home", "Explore", "Compare"])
 
-if len(hs) > 0:
-    f_data = f_data[f_data["household_size"].isin(hs)]
 
-if len(locs) > 0:
-    f_data = f_data[f_data["location"].isin(locs)]
+def switch(tab):
+    return f"""
+var tabGroup = window.parent.document.getElementsByClassName("stTabs")[0]
+var tab = tabGroup.getElementsByTagName("button")
+tab[{tab}].click()
+"""
 
-kwargs = {}
 
-if len(groups) > 0:
-    kwargs["color"] = groups.pop(0)
+with home:
+    # st.header("Welcome to the Solar Shift Explorer!")
+    st.markdown("<h1 style='text-align: center;'>Welcome to the Solar Shift Explorer!</h1>",
+                unsafe_allow_html=True)
+    st.markdown("<h3 style='text-align: center;'>We've simulated the operation of thousands of hot water heaters. </h1>",
+                unsafe_allow_html=True)
+    st.markdown("<h3 style='text-align: center;'>Start exploring and find a system that lowers your bills and carbon emissions.</h1>",
+                unsafe_allow_html=True)
+    st.markdown("<h3 style='text-align: center;'> better intro text/graphics </h1>",
+                unsafe_allow_html=True)
+    a, b, c = st.columns([1.25, 1, 1])
+    with b:
+        if st.button("Explore Now"):
+            with st.empty():
+                html(f"<script>{switch(1)}</script>", height=0)
+                time.sleep(1)
+                html(f"<div></div>", height=0)
 
-if len(groups) > 0:
-    kwargs["symbol"] = groups.pop(0)
+with explore:
+    top = st.container()
+    bottom = st.container()
+    with top:
 
-chart = px.scatter(
-    f_data,
-    x='emissions_total',
-    y='annual_energy_cost',
-    # tooltip=["has_solar", "control_type", "tariff_type"],
-    **kwargs
-)
+        left_col, padding_left, right_col, padding = st.columns([3, 0.5, 7, 0.5])
 
-with middle_col:
-    st.plotly_chart(chart, use_container_width=True)
+        with left_col:
+            with st.expander("Your house", expanded=True):
+                hs = st.multiselect("Household size", data["household_size"].unique(), default=3)
+                locs = st.multiselect("Location", data["location"].unique())
+                tariffs = st.multiselect("Tariff", data["tariff_type"].unique())
+                solar = st.multiselect("Solar", data["has_solar"].unique())
+            with st.expander("Heater choices"):
+                heater = st.multiselect("Heater type", data["heater_type"].unique())
+                control = st.multiselect("Control type", data["control_type"].unique())
+            # with st.expander("Plot comparisons"):
+            #     x = st.selectbox("Side-by-side", groups, index=0)
+            #     color = st.selectbox("Color", groups)
+
+        f_data = data.copy()
+
+        if len(hs) > 0:
+            f_data = f_data[f_data["household_size"].isin(hs)]
+
+        if len(locs) > 0:
+            f_data = f_data[f_data["location"].isin(locs)]
+
+        if len(tariffs) > 0:
+            f_data = f_data[f_data["tariff_type"].isin(tariffs)]
+
+        if len(solar) > 0:
+            f_data = f_data[f_data["has_solar"].isin(solar)]
+
+        if len(control) > 0:
+            f_data = f_data[f_data["control_type"].isin(control)]
+
+        if len(heater) > 0:
+            f_data = f_data[f_data["heater_type"].isin(heater)]
+
+
+    show_data = f_data.loc[:, [
+            "location",
+            "household_size",
+            "heater_type",
+            "tariff_type",
+            "control_type",
+            "has_solar",
+            "annual_energy_cost",
+            "emissions_total"
+        ]
+    ]
+
+
+    with bottom:
+        bl, bm, br, _ = st.columns([2.5, 2, 2, 1])
+        with bm:
+            x = st.selectbox("Side-by-side", groups, index=2)
+        with br:
+            color = st.selectbox("Color", groups, index=2)
+
+
+    with top:
+        chart = px.strip(
+            f_data,
+            y='annual_energy_cost',
+            x=x,
+            color=color
+        )
+        chart.update_traces(width=2.0)
+
+        chart.update_layout(
+            margin={
+                't': 20,
+                'b': 20,
+            }
+        )
+
+        with right_col:
+            st.plotly_chart(chart, use_container_width=True)
+
+
+    with bottom:
+        table_groups = list(set((x, color)))
+        summarise = st.radio("", ["Average", "Show all"])
+        if len(table_groups) > 0 and summarise == "Average":
+            show_data = show_data.groupby(table_groups, as_index=False).agg(
+                {"annual_energy_cost": "mean", "emissions_total": "mean"}
+            )
+        show_data = show_data.sort_values("annual_energy_cost")
+        st.dataframe(show_data.style.format(precision=2), hide_index=True)
